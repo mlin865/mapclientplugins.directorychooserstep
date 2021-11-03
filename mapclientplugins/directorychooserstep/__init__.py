19f7ac8bbfa302d6cfb9629c4d906215b475e9ab
hsorby <h.sorby@auckland.ac.nz>
2021-11-03 20:53:58 +1300

    Set step location metadata.

--- mapclientplugins/directorychooserstep/__init__.py
+++ mapclientplugins/directorychooserstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin
 __version__ = '0.1.0'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'Directory Chooser'
-__location__ = ''
+__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.directorychooserstep'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.directorychooserstep import step
