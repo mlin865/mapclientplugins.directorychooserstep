69255a91bd2e62851c46a597b126e3e99ea5dc95
TimmyNic <tim.nicolas@outlook.com>
2023-01-19 12:02:16 +1300

    Upgrade to PySide6.

--- mapclientplugins/directorychooserstep/step.py
+++ mapclientplugins/directorychooserstep/step.py
@@ -5,7 +5,7 @@ MAP Client Plugin Step
 import os
 import json
 
-from PySide2 import QtGui, QtWidgets
+from PySide6 import QtGui
 
 from mapclient.mountpoints.workflowstep import WorkflowStepMountPoint
 from mapclientplugins.directorychooserstep.configuredialog import ConfigureDialog
